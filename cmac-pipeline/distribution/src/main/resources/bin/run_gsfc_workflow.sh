f05547cb86146fbda1f93ac0cdb9f7562e12da90
cinquini <cinquini@d17342fb-6902-0410-9eac-c5362d96ede4>
2013-08-13 14:53:28 +0000

    Inserting example script to run GSFC workflow.

---
+++ cmac-pipeline/distribution/src/main/resources/bin/run_gsfc_workflow.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# example script to run a full GSFC workflow instance
+
+# empty Solr database
+curl -s http://localhost:8080/solr/update?commit=true -H "Content-Type:text/xml" --data-binary '<delete><query>*:*</query></delete>'
+
+# remove last 2013-03 file from log so it can be downloaded again
+cp /usr/local/pge/GSFC/exec/AIRX3STM.005.history-no-2013-03 /usr/local/pge/GSFC/exec/AIRX3STM.005.history
+
+# run example workflow
+cd /usr/local/cmac/workflow/bin
+./wmgr-client --url http://localhost:9200 --operation --sendEvent --eventName esgf --metaData --key data_node test-datanode.jpl.nasa.gov
