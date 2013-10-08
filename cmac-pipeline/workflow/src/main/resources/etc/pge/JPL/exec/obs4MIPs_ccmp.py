8dd1776475c844e5753e020a222d40e414160cd5
cinquini <cinquini@d17342fb-6902-0410-9eac-c5362d96ede4>
2013-10-08 21:07:38 +0000

    Improving generation of PO.DAAC products

--- cmac-pipeline/workflow/src/main/resources/etc/pge/JPL/exec/obs4MIPs_ccmp.py
+++ cmac-pipeline/workflow/src/main/resources/etc/pge/JPL/exec/obs4MIPs_ccmp.py
@@ -7,10 +7,18 @@ import subprocess
 import ConfigParser
 import datetime
 import uuid
+from collections import OrderedDict
 
+# dictionary that maps the original variable name to the CMIP5 controlled vocabulary name
+# uwnd -> uas, vwnd -> vas, wspd -> sfcWind
+# NOTE: do NOT include variables "upstr", "vpstr"
+#VARIABLES = { "uwnd":"uas", "vwnd":"vas", "wspd":"sfcWind", "nobs":"nobs" }
+VARIABLES = { "uwnd":"uas" }
+variables = OrderedDict(sorted(VARIABLES.items(), key=lambda t: t[0]))
 
-VARIABLES = ["uwnd","vwnd","wspd","upstr","vpstr","nobs"]
-#VARIABLES = ["uwnd"]
+# global attributes to be removed
+#NC_GLOBAL = ["NC_GLOBAL.Conventions", "NC_GLOBAL.title", "NC_GLOBAL.description", "NC_GLOBAL.history", "NC_GLOBAL.base_date",
+#             "DODS_EXTRA.Unlimited_Dimension", "NCO"]
 
 # file containing common (in [default] section) and variable-specific (in [variable] section) global attributes
 CONFIG_FILE = "./obs4MIPs_ccmp.cfg"
@@ -77,13 +85,17 @@ def main():
             # loop over sub-directories i.e. years    
             for file in os.listdir(inSubDir):
                 inFile = os.path.join(inSubDir, file)
+                # "month_20110101_v11l35flk.nc.gz.nc" --> ['month', '20110101', 'v11l35flk.nc.gz.nc']
+                parts = file.split("_")
+                _parts = parts[2].split(".")
+                version = _parts[0]
                 
                 # create output directory if not existing already
                 if not os.path.exists(outSubDir):
                     os.makedirs(outSubDir)
                     print "Created output directory: %s" % outSubDir
                
-                for var in VARIABLES:
+                for var in variables.keys():
                     
                     # initialize list for this variable
                     try:
@@ -106,7 +118,8 @@ def main():
     
                                     
             # concatenate all files for each variable
-            for var in VARIABLES:
+            for var in variables.keys():
+                newvar = variables[var]
                 command = os.path.join(NCOPATH,"ncrcat")
                 command = command + " -h --overwrite"
                 sumFile = os.path.join(outSubDir,"year_%s_v11l35flk_%s.nc" % (year, var))
@@ -121,7 +134,22 @@ def main():
                 for file in monthly_files[var]:
                     os.remove(file)
                     
-                # add global attrbutes from [default] and [variable] sections
+                # rename variable
+                yyyymmdd = datetime.date.today().strftime("%Y%m%D")
+                newSumFile = os.path.join(outSubDir,"%s_ccmp_l35_v20131003_%s01_%s12.nc" % (newvar, year, year))
+                command = os.path.join(NCOPATH,"ncrename")
+                command = command + " -h --overwrite -v %s,%s %s %s" % (var, newvar, sumFile, newSumFile)
+                execute(command)
+                
+                # cleanup
+                os.remove(sumFile)
+                
+                # remove all old global attributes
+                command = os.path.join(NCOPATH, "ncatted")
+                command = command + " -h -a ,global,d,, %s" % newSumFile
+                execute(command)
+                    
+                # add new global attributes from [default] and [variable] sections
                 for key, value in (dict(config.items('default') + config.items(var))).items():
                     
                     # special attribute processing
@@ -133,7 +161,7 @@ def main():
                         value = replace(value, '$UUID', uuid.uuid4().__str__()) # random UUID
                     
                     command = os.path.join(NCOPATH,"ncatted")
-                    command = command + " -h -a '%s',global,c,c,'%s' %s" % (key, value, sumFile)
+                    command = command + " -h -a '%s',global,c,c,'%s' %s" % (key, value, newSumFile)
                     execute(command)
 
             # write out this year
