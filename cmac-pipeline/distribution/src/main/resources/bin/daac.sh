7c91858be8ba0f863d9cd0a805a0586c1d8d247a
mjjoyce <mjjoyce@d17342fb-6902-0410-9eac-c5362d96ede4>
2013-12-04 23:21:14 +0000

    daac specific env settings

---
+++ cmac-pipeline/distribution/src/main/resources/bin/daac.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# CMAC environment configuration
+
+# Choose an identifier for the DAAC (e.g. GSFC)
+#export DAAC_ID=GSFC
+
+# The https-based URL of the TDS serving the data
+#export TDS_HTTPS_URL=https://localhost:8443/thredds
+
+# The digest username to re-initialize the TDS, matching the value specified in tomcat-users.xml
+#export TDS_USERNAME=****
+
+# The digest password to reinitializet the TDS, matching the value specified in tomcat-users.xml
+#export TDS_PASSWORD=****
+
+
+# server name and port of local ESGF data node serving the data
+#export DATA_NODE=localhost:8080
+
+# server name and port of target ESGF Index Node indexing the data
+#export INDEX_NODE=test-datanode.jpl.nasa.gov
