9a29dfdc48baec9cc07a9d526eb75476586aebe4
cinquini <cinquini@d17342fb-6902-0410-9eac-c5362d96ede4>
2013-12-17 21:28:21 +0000

    Updating example script.

--- cmac-pipeline/distribution/src/main/resources/bin/run_gsfc_workflow.sh
+++ cmac-pipeline/distribution/src/main/resources/bin/run_gsfc_workflow.sh
@@ -5,8 +5,8 @@
 curl -s http://localhost:8080/solr/update?commit=true -H "Content-Type:text/xml" --data-binary '<delete><query>*:*</query></delete>'
 
 # remove last 2013-03 file from log so it can be downloaded again
-cp /usr/local/pge/GSFC/exec/AIRX3STM.005.history-no-2013-03 /usr/local/pge/GSFC/exec/AIRX3STM.005.history
+cp /usr/local/pge/exec/AIRX3STM.005.history-no-2013-03 /usr/local/pge/exec/AIRX3STM.005.history
 
 # run example workflow
 cd /usr/local/cmac/workflow/bin
-./wmgr-client --url http://localhost:9200 --operation --sendEvent --eventName esgf --metaData --key data_node test-datanode.jpl.nasa.gov
+./wmgr-client --url http://localhost:9001 --operation --sendEvent --eventName esgf --metaData --key data_node test-datanode.jpl.nasa.gov
