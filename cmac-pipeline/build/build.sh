df79849c26f88df5e8daabf97a13e24c2c1cc805
ahart <ahart@d17342fb-6902-0410-9eac-c5362d96ede4>
2013-12-19 00:10:31 +0000

    Install esgf-search jar into .m2 repo before attempting to build

--- cmac-pipeline/build/build.sh
+++ cmac-pipeline/build/build.sh
@@ -13,14 +13,22 @@ CWD=$(pwd)
 SRC=`dirname $CWD`
 VERSION=0.2.0-beta
 
-# 1. Build the CMAC-Extensions component
+# 1. Install the ESG-Search Jar
+mvn install:install-file \
+  -DgroupId=esgf.org \
+  -DartifactId=esg-search \
+  -Dversion=3.7.7 \
+  -Dpackaging=jar \
+  -Dfile=$SRC/distribution/src/main/resources/esgf/esg-search-3.7.7.jar
+
+# 2. Build the CMAC-Extensions component
 cd $SRC/extensions
 mvn package install
-# 2. Build the full CMAC distribution
+# 3. Build the full CMAC distribution
 cd $SRC
 mvn package install
 
-# 3. Package PGE bin scripts into final distribution.
+# 4. Package PGE bin scripts into final distribution.
 # TODO: For a future release, this step could be folded into the
 #       maven build step above.
 BUILD_DIR=$SRC/distribution/target/cmac-pipeline-$DAAC_ID-$VERSION
